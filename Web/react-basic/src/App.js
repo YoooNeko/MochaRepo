da69ed17cb15a4d0d736737c80fb517924ab0117
YoooNeko <yooo_fyy@163.com>
2023-02-20 18:03:09 +0800

    React

--- Web/react-basic/src/App.js
+++ Web/react-basic/src/App.js
@@ -1,34 +1,13 @@
 import './app.css';
 import React from "react"
 
-// 函数组件的创建和渲染
-// 创建 (组件首字母大写)
-// 函数组件必须有返回值
-// 渲染 <Hello/>    <Hello></Hello>
-function Hello() {
-  const clickHandler = () => {
-    console.log('函数组件的事件被触发了');
-  }
-  return <div onClick={clickHandler}>hello</div>
-}
 
-// 类组件的创建和渲染
-class WorldComponent extends React.Component {
-  clickHandler = () => {
-    console.log('类组件的事件被触发了')
-  }
-  render() {
-    return <div onClick={this.clickHandler}>this is class Component</div>
-  }
-}
 
+// 根组件
 function App() {
   return (
     <div className="App">
-      <Hello/>
-      <Hello></Hello>
-      <WorldComponent/>
-      <WorldComponent></WorldComponent>
+
     </div>
   );
 }
@@ -36,6 +15,3 @@ function App() {
 export default App;
 
 // 结尾笔记
-// HTML标签记录
-// 1. p标签：该行会创建一些空白
-// 2. 
